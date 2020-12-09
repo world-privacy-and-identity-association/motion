f8e8ef8206a3fa628c66d5a2a03b11bd80bc7a46
INOPIAE <m.maengel@inopiae.de>
2020-12-09 22:30:27 +0100

    upd: fixes test_user_api.py

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -1,4 +1,4 @@
-from .test_basics import BasicTest
+from tests.test_basics import BasicTest
 import click
 import motion
 import postgresql
@@ -26,12 +26,10 @@ class GeneralTests(BasicTest):
         user = 'John Doe'
         runner = CliRunner()
         result = runner.invoke(create_user, [user])
-        assert result.exit_code == 0
         self.assertIn("User 'John Doe' inserted.", result.output)
 
         rv = db_select(self,"SELECT email FROM voter WHERE lower(email)=lower($1)", user)
         self.assertIn(user, rv[0].get("email"))
 
         result = runner.invoke(create_user, [user])
-        assert result.exit_code == 0
         self.assertIn("User 'John Doe' already exists.", result.output)
